e0ea06eeb00df2c4d48051ff3f942b5ea715cc94
Jack H <jackhe678@gmail.com>
2017-03-02 22:12:53 -0500

    Project 22-25 completed and added

--- 24_Lexicographic_Permutations.rb
+++ 24_Lexicographic_Permutations.rb
@@ -1,8 +1,48 @@
-Lexicographic permutations
-Problem 24
+# Lexicographic permutations
+# Problem 24
+#
+# A permutation is an ordered arrangement of objects.
+# For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+# If all of the permutations are listed numerically or alphabetically,
+# we call it lexicographic order.
+# The lexicographic permutations of 0, 1 and 2 are:
+#
+# 012   021   102   120   201   210
+#
+# What is the millionth lexicographic permutation of the
+# digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
-A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+def perm(arr)
+  digits = arr.permutation.to_a
+  digits[999_999].join
+end
 
-012   021   102   120   201   210
+digits = (0..9).to_a
+puts perm(digits)
 
-What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+#### Above, with permutation method ####
+#### Below, a cleaver solution (Credits to Bristow)####
+#### http://jondotcomdotorg.net/ #####
+
+class Integer
+  def factorial
+    return 1 if self == 0
+    (1..self).inject(1,:*)
+  end
+end
+
+def solver
+  seed = "0123456789"
+  sofar = ""
+  nth = 999999
+  (0..9).each do |i|
+    f=(9-i).factorial
+    n=nth/f
+    sofar += seed[n]
+    nth -= f*n
+    seed = seed.delete(seed[n])
+  end
+  sofar
+end
+
+puts solver
--- 25_1000-digit_Fibonacci_Number.rb
+++ 25_1000-digit_Fibonacci_Number.rb
@@ -1,24 +1,66 @@
-1000-digit Fibonacci number
-Problem 25
+# 1000-digit Fibonacci number
+# Problem 25
+#
+# The Fibonacci sequence is defined by the recurrence relation:
+#
+#     Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+#
+# Hence the first 12 terms will be:
+#     F1 = 1
+#     F2 = 1
+#     F3 = 2
+#     F4 = 3
+#     F5 = 5
+#     F6 = 8
+#     F7 = 13
+#     F8 = 21
+#     F9 = 34
+#     F10 = 55
+#     F11 = 89
+#     F12 = 144
+#
+# The 12th term, F12, is the first term to contain three digits.
+#
+# What is the index of the first term in the Fibonacci sequence to contain
+# 1000 digits?
 
-The Fibonacci sequence is defined by the recurrence relation:
+# I used the winning Fibonacci algorithm; tested at "./files/fibonacci.rb"
 
-    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+def fibonacci(n)
+  a, b = 0, 1
 
-Hence the first 12 terms will be:
+  n.times do
+    temp = a
+    a = b
+    b = temp + b
+  end
 
-    F1 = 1
-    F2 = 1
-    F3 = 2
-    F4 = 3
-    F5 = 5
-    F6 = 8
-    F7 = 13
-    F8 = 21
-    F9 = 34
-    F10 = 55
-    F11 = 89
-    F12 = 144
+  a
+end
 
-The 12th term, F12, is the first term to contain three digits.
+def fib1kDigit
+  i = 12
 
+  while i > 0
+    if fibonacci(i).to_s.chars.length >= 1000
+      return i
+    end
+    i += 1
+  end
+end
+
+puts fib1kDigit
+
+#### Below, Johnny thought of a cleaver solution ####
+#### http://johnnycoder.com/blog/2010/07/19/project-euler-25-ruby/ ####
+
+
+answer = -1
+previous = 0
+i = 1
+while i.to_s.length < 1000
+    answer += 1
+    i, previous = previous, previous + i
+end
+
+puts answer
